c10fdb6c8b0653a29e1c590c82c9ca557417e162
kitmarrack <rlkl7110@leeds.ac.uk>
2025-10-28 14:09:54 +0000

    9.4 task 3.1 Complete

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,15 @@
 // Task 3.1: command line arguments
+
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        println("2 Arguments must be supplied")
+        exitProcess(1)
+    } else {
+        println(args[0])
+        println(args[1])
+        exitProcess(0)
+    }
+    
+}
