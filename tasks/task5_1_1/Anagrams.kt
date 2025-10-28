4d6ac8c513faadc319c392550ef4a34e9dd8edca
kitmarrack <rlkl7110@leeds.ac.uk>
2025-10-28 18:36:16 +0000

    10.0 Task 5.1.2 Complete

--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,17 @@
 // Task 5.1.1: anagram checking using a function
+fun anagrams(first: String, second: String): Boolean {
+    if (first.length != second.length) {
+        return false
+    }
+    val firstChars = first.lowercase().toList().sorted()
+    val secondChars = second.lowercase().toList().sorted()
+    return firstChars == secondChars
+}
+
+fun main () {
+    print("Enter your first string: ")
+    val first = readln()
+    print("Enter your first string: ")
+    val second = readln()
+    println(anagrams(first, second))
+}
--- tasks/task5_1_2/Die.kt
+++ tasks/task5_1_2/Die.kt
@@ -1,1 +1,26 @@
 // Task 5.1.2: die rolling simulation
+import kotlin.random.Random
+
+fun rollDie(sides: Int) {
+    if (sides in setOf(4, 6, 8, 10, 12, 20)) {
+        println("Rolling a d$sides...")
+        val result = Random.nextInt(1, sides + 1)
+        println("You rolled $result")
+    }
+    else {
+        println("Error: cannot have a $sides-sided die")
+    }
+}
+
+fun readInt(prompt: String): Int {
+    print(prompt)
+    var sides = readln().toInt()
+    return sides
+}
+
+fun main () {
+    val prompt = ("Enter dice side number: ")
+    rollDie(readInt(prompt))
+}
+
+
