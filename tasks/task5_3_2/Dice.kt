6ab4c1b668bf47426dc250d7ce9423b37b7ab62e
kitmarrack <rlkl7110@leeds.ac.uk>
2025-11-04 17:22:59 +0000

    11.0 Task 5.4.2 Complete

--- tasks/task5_3_2/Dice.kt
+++ tasks/task5_3_2/Dice.kt
@@ -1,1 +1,34 @@
 // Task 5.3.2: dice rolling simulation
+import kotlin.random.Random
+
+fun rollDice(sides: Int = 6, noDice: Int = 1) {
+    if (sides in setOf(4, 6, 8, 10, 12, 20)) {
+        var total = 0
+        repeat(noDice) {
+            println("Rolling a d$sides...")
+            val result = Random.nextInt(1, sides + 1)
+            println("You rolled $result")
+            total += result
+        }
+        println("Your total is $total")
+    }
+    else {
+        println("Error: cannot have a $sides-sided die")
+    }
+}
+
+
+fun main() {
+    println("Enter the number of sides or leave blank for 6: ")
+    val sides = readln()
+    println("Enter the number of dice or leave blank for 1: ")
+    val noDice = readln()
+
+    when {
+    sides.isBlank() && noDice.isBlank() -> rollDice()
+    sides.isNotBlank() && noDice.isBlank() -> rollDice(sides.toInt())
+    sides.isBlank() && noDice.isNotBlank() -> rollDice(noDice.toInt())
+    else -> rollDice(sides.toInt(), noDice.toInt())
+    }
+}
+
--- tasks/task5_4_2/StringProp.kt
+++ tasks/task5_4_2/StringProp.kt
@@ -1,10 +1,9 @@
-// Task 5.4.1: string extension function
-fun String.tooLong() = this.length > 20
+val String.tooLong: Boolean get() = this.length > 20
 
 fun main() {
     println("Enter your string: ")
     val str = readln()
-    if (str.tooLong()) {
+    if (str.tooLong) {
         println("This string is too long!")
     } else {
         println("This string is fine")
