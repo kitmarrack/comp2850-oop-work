ca823955b46de92f1d6093f3557d148cb835996c
kitmarrack <rlkl7110@leeds.ac.uk>
2025-11-15 19:42:26 +0000

    14.0 Completed Wordle, still need to do gradle tests

--- portfolio/wordle/src/main/kotlinFiles/Main.kt
+++ portfolio/wordle/src/main/kotlinFiles/Main.kt
@@ -1,9 +1,18 @@
-package kotlinFiles
+
 fun main() {
+    val words = readWordList("data/words.txt")
+    val target = pickRandomWord(words)
+    println(target)
     var attempt = 0
-    while (attempt < 10) {
-        obtainGuess(attempt)
+    while (attempt <= 10) {
+        val guess = obtainGuess(attempt)
+        val check = evaluateGuess(guess, target)
+        displayGuess(guess, check)
         attempt += 1
+        if (guess == target) {
+            println("\nCorrect!")
+            break
+        }        
     }
 
 }
--- portfolio/wordle/src/main/kotlinFiles/Wordle.kt
+++ portfolio/wordle/src/main/kotlinFiles/Wordle.kt
@@ -1,62 +1,60 @@
 // Implement the six required functions here
-package kotlinFiles
-
 import java.io.File
 
 fun isValid(word: String): Boolean {
+    if (word.length == 5) {
+        return true
+    } else {
+        return false
+    }
 }
 
 fun readWordList(filename: String): MutableList<String> {
-    val wordsFile = "data/words.txt" //opens the text file with words stored
-    val wordArray: Array<String> = File(wordsFile).readlines().toTypedArray() //reads the lines and stores all the words in an array
+    val words: MutableList<String> = File(filename).readLines().toMutableList()
+    return words
 }
 
 fun pickRandomWord(words: MutableList<String>): String {
-    val randomWord = wordArray.random()
+    val randomWord = words.random()
     return randomWord
 }
 
 fun obtainGuess(attempt: Int): String {
-    println("Enter your guess ")
-    var guess = readLine()
+    var guess: String
+    do {
+        print("\nAttempt $attempt: ")
+        guess = readln().uppercase()
+    } while (isValid(guess) == false)
     return guess
 }
 
+
 fun evaluateGuess(guess: String, target: String): List<Int> {
-    var guess = guess.toTypedArray()
-    var randomWord = randomWord.toTypedArray()
-    var check = arrayOf('0','0,'0','0','0')
-
-    for (i in 0,4) {
-        if guess[i] == randomWord[i] {
-            check[i] = 2
-        }
-    }
-
-    for (i in 0,4) {
-        if check[i] != 2 {
-            for j(0,4) {
-                if guess[i] == randomWord[j] {
-                    check[i] = 1
-                }
+    var check: MutableList<Int> = mutableListOf(0,0,0,0,0)
+    for (i in 0..4) {
+        if (guess[i] in target) {
+            if (guess[i] == target[i]) {
+                check[i] = 2
+            } else {
+                check[i] = 1
             }
         }
     }
-   
-
+    return check
 }
         
-fun displayGuess(guess: String, matches: List<Int>) {
-    val green = "\u001b[32m"
-    val orange = "\u001b[38;5;208m"
-    val reset = "\u001b[0m"
-    for (i in 0,4) {
-        if check[i] == 0 {
-            println(guess[i])
-        } else if check[i] == 1 {
-            println(orange + guess[i] + reset)
+fun displayGuess(guess: String, check: List<Int>) {
+    val reset = "\u001B[0m"
+    val green = "\u001B[32m"
+    val yellow = "\u001B[33m"
+    //var displayList: MutableList<String> = mutableListOf("?","?","?","?","?")
+    for (i in 0..4) {
+        if (check[i] == 2) {
+            print("$green${guess[i]}$reset")
+        } else if (check[i] == 1) {
+            print("$yellow${guess[i]}$reset")
         } else {
-            println(green + guess[i] + reset)
+            print(guess[i])
         }
     }
 }
