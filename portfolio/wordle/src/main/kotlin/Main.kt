ecef1fef3351206008e20b132849ae86c7d06be4
kitmarrack <rlkl7110@leeds.ac.uk>
2025-12-05 13:36:51 +0000

    Wordle Draft 2

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,18 +1,18 @@
 package wordle
+
 fun main() {
-    val words = readWordList("data/words.txt") //calls readWordList, assigning its contents to a mutable list called words
-    val target = pickRandomWord(words) //calls pickRandomWord on words, choosing a random word and assigning it to target
-    var attempt = 0 //creating a pointer value 
-    while (attempt <= 6) { //loop allowing for 6 goes
-        val guess = obtainGuess(attempt) //calling obtain guess which in turn calls isValid and assigning the resulting input to guess
-        val check = evaluateGuess(guess, target) //creating the check list by calling evaluateGuess on guess and target
-        displayGuess(guess, check) //using check to display the accuracy of the users guess
-        attempt += 1 
+    val words = readWordList("data/words.txt") // assigning values in words.txt to words
+    val target = pickRandomWord(words) // assigning random word form words to target
+    var attempt = 0 // creating a pointer value
+    while (attempt <= 6) { // loop allowing for 6 goes
+        val guess = obtainGuess(attempt) // assigning valid guess from obtainGuess function and isValid function
+        val check = evaluateGuess(guess, target) // creating the check list by calling evaluateGuess on guess and target
+        displayGuess(guess, check) // using check to display the accuracy of the users guess
+        attempt += 1
         if (guess == target) {
             println("\nCorrect!")
-            break //breaking out of the loop as soon as the guess is correct
-        }        
+            break // breaking out of the loop as soon as the guess is correct
+        }
     }
     println("\nThe word was $target")
-
 }
--- portfolio/wordle/src/test/kotlin/WordleTest.kt
+++ portfolio/wordle/src/test/kotlin/WordleTest.kt
@@ -1,77 +1,79 @@
 package wordle
-import java.io.File
-import java.io.FileNotFoundException
+
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
 import io.kotest.matchers.collections.shouldBeIn
-import io.kotest.assertions.throwables.shouldThrow
-
-
+import io.kotest.matchers.shouldBe
+import java.io.File
 
 @Suppress("unused")
-class WordleTest : StringSpec({
-    // Write your tests here
+class WordleTest : StringSpec(
+    {
+        // isValid
+        "Not isValid for a string that is longer than 5 characters" {
+            withClue("hellol should not be isValid") { isValid("hellol") shouldBe false }
+        }
+        "Not isValid for a string that is shorter than 5 characters" {
+            withClue("hell should not be isValid") { isValid("hell") shouldBe false }
+        }
+        "isValid for a string that is exactly 5 characters" {
+            withClue("hello should be isValid") { isValid("hello") shouldBe true }
+        }
+        "Not isValid for a string that contains a special character" {
+            withClue("hell! should not be isValid") { isValid("hell!") shouldBe false }
+        }
+        "Not isValid for a string that contains a number" {
+            withClue("hell0 should not be isValid") { isValid("hell0") shouldBe false }
+        }
+        "isValid for a string that contains only lowercase letter characters" {
+            withClue("hello should be isValid") { isValid("hello") shouldBe true }
+        }
+        "isValid for a string that contains only uppercase letter characters" {
+            withClue("HELLO should be isValid") { isValid("HELLO") shouldBe true }
+        }
+        "isValid for a string that contains a mix of both lower and uppercase characters" {
+            withClue("hElLo should be isValid") { isValid("hElLo") shouldBe true }
+        }
 
-    //isValid
-    "Not isValid for a string that is longer than 5 characters" {
-        withClue("hellol should not be isValid") {isValid("hellol") shouldBe false}
-    }
-    "Not isValid for a string that is shorter than 5 characters" {
-        withClue("hell should not be isValid") {isValid("hell") shouldBe false}
-    }
-    "isValid for a string that is exactly 5 characters" {
-        withClue("hello should be isValid") {isValid("hello") shouldBe true}
-    }
-    "Not isValid for a string that contains a special character" {
-        withClue("hell! should not be isValid") {isValid("hell!") shouldBe false}
-    }
-    "Not isValid for a string that contains a number" {
-        withClue("hell0 should not be isValid") {isValid("hell0") shouldBe false}
-    }
-    "isValid for a string that contains only lowercase letter characters" {
-        withClue("hello should be isValid") {isValid("hello") shouldBe true}
-    }
-    "isValid for a string that contains only uppercase letter characters" {
-        withClue("HELLO should be isValid") {isValid("HELLO") shouldBe true}
-    }
-    "isValid for a string that contains a mix of both lower and uppercase characters" {
-        withClue("hElLo should be isValid") {isValid("hElLo") shouldBe true}
-    }
+        // readWordList
+        "mutable list of strings for readWordList of non empty text file" {
+            val nonEmpty = File("data/nonEmpty.txt")
+            nonEmpty.writeText("about\nbroke\ncrate")
+            withClue("readWordList of nonEmpty text file should return mutable list of strings") {
+                readWordList("data/nonEmpty.txt") shouldBe mutableListOf("about", "broke", "crate")
+            }
+        }
+        "empty mutable list for readWordList of empty text file" {
+            val empty = File("data/empty.txt")
+            empty.writeText("")
+            withClue("readWordList of empty text file should return empty mutable list") {
+                readWordList("data/empty.txt") shouldBe mutableListOf()
+            }
+        }
 
-    //readWordList
-    "mutable list of strings for readWordList of non empty text file" {
-        val nonEmpty = createTempFile()
-        nonEmpty.writeText("about\nbroke\ncrate")
-        withClue("readWordList of nonEmpty text file should return mutable list of strings") {readWordList(nonEmpty.path) shouldBe mutableListOf("about","broke","crate")}    
-    }
-    "empty mutable list for readWordList of empty text file" {
-        val empty = createTempFile()
-        empty.writeText("")
-        withClue("readWordList of empty text file should return empty mutable list") {readWordList(empty.path) shouldBe mutableListOf()}
-    }
-    "FileNotFoundException for readWordList of non existent file" {
-        withClue("readWordList of non existent text file should return FileNotFoundException") {shouldThrow<FileNotFoundException> {readWordList("doesNotExist.txt")}} 
-    }
-
-    //pickRandomWord
-    "string that exists in list for pickRandomWord of non empty mutable list of strings, also only a single string returned" {
-        val nonEmpty = mutableListOf("about","broke","crate")
-        withClue("pickRandomWord of non empty mutable list should return a single string") {pickRandomWord(nonEmpty) shouldBeIn nonEmpty }
-    }
-    "NoSuchElementException for pickRandomWord of empty list" {
-        val empty = mutableListOf<String>()
-        withClue("pickRandomWord of empty mutable list should return NoSuchElementException") {shouldThrow<NoSuchElementException> {pickRandomWord(empty)}}
-    }
-    //evaluateGuess
-    "characters in check are 2 when the same characters in guess and target are equal" {
-        withClue("evaluateGuess of hello and hello should make check = [2,2,2,2,2]") {evaluateGuess("hello","hello") shouldBe listOf(2,2,2,2,2)}
-    }
-    "characters in check are 1 when the same character guess exists in target in a different position" {
-        withClue("evaluateGuess of hello and lohel should make check = [1,1,1,1,1]") {evaluateGuess("hello","lohel") shouldBe listOf(1,1,1,1,1)}
-    }
-    "characters in check are 0 when the same character in guess doesnt appear in target" {
-        withClue("evaluateGuess of abcde and fghij should make check = [0,0,0,0,0]") {evaluateGuess("abcde","fghij") shouldBe listOf(0,0,0,0,0)}
-    }
-}
+        // pickRandomWord
+        "single string that exists in nonEmpty for pickRandomWord of non empty list" {
+            val nonEmpty = mutableListOf("about", "broke", "crate")
+            withClue("pickRandomWord of non empty mutable list should return a single string") {
+                pickRandomWord(nonEmpty) shouldBeIn nonEmpty 
+            }
+        }
+        // evaluateGuess
+        "characters in check are 2 when the same characters in guess and target are equal" {
+            withClue("evaluateGuess of hello and hello should make check = [2,2,2,2,2]") {
+                evaluateGuess("hello", "hello") shouldBe listOf(2, 2, 2, 2, 2)
+            }
+        }
+        "characters in check are 1 when the same character guess exists in target in a different position" {
+            withClue("evaluateGuess of hello and lohel should make check = [1,1,1,1,1]") {
+                evaluateGuess("hello", "lohel") shouldBe listOf(1, 1, 1, 1, 1)
+            }
+        }
+        "characters in check are 0 when the same character in guess doesnt appear in target" {
+            withClue("evaluateGuess of abcde and fghij should make check = [0, 0, 0, 0, 0]") {
+                evaluateGuess("abcde", "fghij") shouldBe listOf(0, 0, 0, 0, 0)
+            }
+        }
+    },
 )
